79f7660efc69ce216be807178ff43a27048d222c
Gabos51 <adebayojamiu51@gmail.com>
2022-11-02 10:12:07 +0100

    commit initial files

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let scoreLog = document.getElementById("num1-el")
+
+let num1 = 0
+function addOne() {
+    num1 += 1
+    scoreLog.textContent = num1
+}
+
+function addTwo() {
+    num1 += 2
+    scoreLog.textContent = num1
+}
+
+function addThree() {
+    num1 += 3
+    scoreLog.textContent = num1
+}
+
+
+let scoreCount = document.getElementById("num2-el")
+
+let num2 = 0
+function addOne1() {
+    num2 += 1
+    scoreCount.textContent = num2
+}
+
+function addTwo1() {
+    num2 += 2
+    scoreCount.textContent = num2
+}
+
+function addThree1() {
+    num2 += 3
+    scoreCount.textContent = num2
+}
+
